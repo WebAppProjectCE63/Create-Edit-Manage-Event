089a239cee050850a2b0e51a6c78b5185985a0d7
SettapongKeawnok <settapong.24050@gmail.com>
2026-02-18 01:43:50 +0700

    #Add Login/Signup Home MyyEvent Page

---
+++ WebApplicationProject/Views/Home/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationProject.Views.Home
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
